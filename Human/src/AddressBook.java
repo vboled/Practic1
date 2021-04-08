ea3bbd801e084f8e84bae55e63daa8f61b9d2178
Кирилл Голубенко <vboled@gmail.com>
2021-04-08 22:05:17 +0300

    AddressBook complete

--- Human/src/AddressBook.java
+++ Human/src/AddressBook.java
@@ -12,7 +12,7 @@ public class AddressBook {
                 '}';
     }
 
-    public static Calendar getDate(String[] mas) {
+    public static Calendar getDate(String[] mas) {                                  // Парсим mas, который содержит одну запись в файле addressBook.txt
         String[] date = mas[2].split("\\.");
         return new GregorianCalendar(Integer.parseInt(date[2]),
                 Integer.parseInt(date[1]) - 1, Integer.parseInt(date[0]));
@@ -23,39 +23,37 @@ public class AddressBook {
                 Integer.parseInt(mas[6]));
     }
 
-    public void printAddressBook() {
-        for (Human man : addressBook) {
-            System.out.println(man.getName() + " " + man.getSurname() + " " +
-                    man.getAddress().toString() + " " + man.getBirthDate().getTime());
-        }
+    public ArrayList<Human> getNeighbors(String street) {                   // Находим всех людей, живущих на одной улице
+        return new ArrayList<Human>(addressBook.stream().filter(u ->
+                u.getAddress().getStreet().equals(street)).collect(Collectors.toList()));
     }
 
-    public ArrayList<Human> getHumansByAddress(Address address) {
+    public ArrayList<Human> getHumansByAddress(Address address) {           // Находим всех людей живущих по переданному адресу
         return new ArrayList<Human>(addressBook.stream().filter(u ->
                 u.getAddress().equals(address)).collect(Collectors.toList()));
     }
 
-    public ArrayList<Human> getOldestHuman() {
+    public ArrayList<Human> getOldestHuman() {                              // Находим старейшего человека в адресной книге
         Comparator<Human> comparator = Comparator.comparing(obj -> obj.getBirthDate());
         Calendar oldestDate = Collections.min(addressBook, comparator).getBirthDate();
         return new ArrayList<Human>(addressBook.stream().filter(u ->
                 u.getBirthDate().equals(oldestDate)).collect(Collectors.toList()));
     }
 
-    public ArrayList<Human> getHumansByDates(Calendar begin, Calendar end) {
+    public ArrayList<Human> getHumansByDates(Calendar begin, Calendar end) { // Находим всех людей, родившихся в интервале от begin до end
         return new ArrayList<Human>(addressBook.stream().filter(u ->
                 u.getBirthDate().getTime().after(begin.getTime()) &&
                         u.getBirthDate().getTime().before(end.getTime())).collect(Collectors.toList()));
     }
 
-    public ArrayList<Human> getYoungestHuman() {
+    public ArrayList<Human> getYoungestHuman() {                             // Находим самого молодого человека в адресной книге
         Comparator<Human> comparator = Comparator.comparing(obj -> obj.getBirthDate());
         Calendar youngestDate = Collections.max(addressBook, comparator).getBirthDate();
         return new ArrayList<Human>(addressBook.stream().filter(u ->
                 u.getBirthDate().equals(youngestDate)).collect(Collectors.toList()));
     }
 
-    public ArrayList<Human> getHumansBySurname(String surname) {
+    public ArrayList<Human> getHumansBySurname(String surname) {            // Находим всех людей по переданной фамилии
         return new ArrayList<Human>(addressBook.stream().filter(u ->
                 u.getSurname().equals(surname)).collect(Collectors.toList()));
     }
--- Human/src/Main.java
+++ Human/src/Main.java
@@ -5,13 +5,57 @@ import java.util.GregorianCalendar;
 public class Main {
     public static void main(String[] argv) {
         try {
-            AddressBook addressBook = new AddressBook("./source/addressBook.txt");
-            Calendar begin = new GregorianCalendar(1950, 0, 10);
-            Calendar end = new GregorianCalendar(2020, 0, 10);
+            AddressBook addressBook = new AddressBook("./source/addressBook.txt"); //fill addressBook
+            // test getHumansByDates(begin, end);
+            System.out.println("Example of work \"getHumansByDates\": ");
+            Calendar begin = new GregorianCalendar(1950, Calendar.JANUARY, 10);
+            Calendar end = new GregorianCalendar(2020, Calendar.MARCH, 10);
             ArrayList<Human> res = addressBook.getHumansByDates(begin, end);
             for (Human h : res) {
-                System.out.println(h.getName());
+                System.out.println(h.toString());
             }
+            System.out.println();
+
+            // test getNeighbors
+            System.out.println("Example of work \"getNeighbors\": ");
+            res = addressBook.getNeighbors("Kosmonavtov");
+            for (Human h : res) {
+                System.out.println(h.toString());
+            }
+            System.out.println();
+
+            // test getHumansBySurname
+            System.out.println("Example of work \"getHumansBySurname\": ");
+            res = addressBook.getHumansBySurname("Ivanov");
+            for (Human h : res) {
+                System.out.println(h.toString());
+            }
+            System.out.println();
+
+            // test getOldestHuman
+            System.out.println("Example of work \"getOldestHuman\": ");
+            res = addressBook.getOldestHuman();
+            for (Human h : res) {
+                System.out.println(h.toString());
+            }
+            System.out.println();
+
+            // test getYoungestHuman
+            System.out.println("Example of work \"getYoungestHuman\": ");
+            res = addressBook.getYoungestHuman();
+            for (Human h : res) {
+                System.out.println(h.toString());
+            }
+            System.out.println();
+
+            // test getHumansByAddress
+            System.out.println("Example of work \"getHumansByAddress\": ");
+            Address address = new Address("Moscow", "Kosmonavtov", 35, 11);
+            res = addressBook.getHumansByAddress(address);
+            for (Human h : res) {
+                System.out.println(h.toString());
+            }
+            System.out.println();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
